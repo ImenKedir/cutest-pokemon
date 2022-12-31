dbc73a48067d7d2ce76aa7f227afca61e1e10568
AnaxGotJuice <64612032+ImenKedir@users.noreply.github.com>
2022-12-31 00:40:41 -0800

    added custom colors to tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,9 @@ const Home: NextPage = () => {
       <div className="text-2xl text-center">Which Pokémon is cuter?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between items-center">
-        <div className="h-16 w-16 bg-red-500" />
+        <div className="h-16 w-16 bg-blue" />
         <div className="p-8">Vs</div>
-        <div className="h-16 w-16 bg-red-500" />
+        <div className="h-16 w-16 bg-blue" />
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      tan: "#e1dfdd",
+      brown: "#50372b",
+      blue: "#809da1",
+    },
   },
   plugins: [],
 };
